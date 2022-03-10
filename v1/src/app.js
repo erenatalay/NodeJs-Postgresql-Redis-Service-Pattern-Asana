185181271757702795afc517e28e5041eb8d4e78
Ahmet Eren ATALAY <61461370+erenatalay@users.noreply.github.com>
2022-03-10 21:47:53 +0300

    static file url

--- v1/src/app.js
+++ v1/src/app.js
@@ -5,11 +5,12 @@ const routers = require("./api-routes");
 const loaders = require("./loaders");
 const events = require("./scripts/events");
 const fileUpload = require("express-fileupload");
+const path = require("path")
 config();
 loaders();
 events();
 const app = express();
-
+app.use("/uploads",express.static(path.join(__dirname,"./","uploads")));
 app.use(express.json());
 app.use(helmet());
 app.use(fileUpload());
